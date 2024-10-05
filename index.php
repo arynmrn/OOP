7ce8237a0127ea2c244e25502335c0bebed10d23
Aryan Emrany <ayanemrany@gmail.com>
2024-10-05 17:31:27 +0330

    change index

--- index.php
+++ index.php
@@ -17,12 +17,15 @@ class MyClass{
 }
 
 $obj = new MyClass;
+$obj2 = new MyClass();
 
 echo $obj->getName();
 
 $obj->setvalue("Arcoo");
+$obj2->setvalue("arcoo.ir");
 
 echo $obj->getName();
+echo $obj2->getName();
 
 
 ?>
